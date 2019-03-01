978d9cf5973382deee5cef744ce48f0d7b2c85b5
Wojciech Bubolka <bubolka@gmail.com>
2019-03-01 00:37:04 +0000

    weather blocks

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react';
 import { geolocated } from 'react-geolocated';
+import WeatherBlock from './WeatherBlock';
 import './WeatherApp.css';
 
 class WeatherApp extends Component {
     constructor() {
         super();
+        this.state = {
+            value: 'Bradford',
+            loading: false,
+            loaded: false,
+            data: {},
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    getLocationData() {
+        const api_key = '5e63f64940665bdc51aa275592b924a4';
+        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${this.state.value}&units=metric&lang=pl&appid=${api_key}`;
+        this.setState({ loading: true });
+        setTimeout(() => {
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => this.setState({ data: data, loading: false, loaded: true }))
+                .catch((e) => console.log('api error', e));
+        }, 300);
+    }
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.getLocationData();
+    }
+    renderLoading() {
+        if (this.state.loading) {
+            return (
+                <div className="loading col-12 align-item-center">
+                    <i className={`fas fa-sync animated slower ${this.state.loading ? 'fadeIn' : 'fadeOut'}`}></i>
+                </div>
+            );
+        }
+    }
+    renderWeather() {
+        if (!this.state.loading && this.state.data) {
+
+        }
+    }
+    renderWeatherBlocks() {
+        if (!this.state.loading && this.state.data && this.state.data.list) {
+            console.log(this.state.data.list);
+            return this.state.data.list.map((item, index) => <WeatherBlock key={index} item={item} />);
+        }
     }
     render() {
         return (
             <div className="WeatherApp">
-                { this.props.isGeolocationAvailable ? `yes` : `no`}
+                {console.log(this.props)}
+                {this.props.isGeolocationAvailable ? `yes` : `no`}
                 <header className="WeatherApp-header">
                     <div className="container">
-                        <div className="row">
-                            <div className="col-6">
+                        <div className="row align-items-center">
+                            <div className="col-12 col-md-6">
                                 <div className="WeatherApp-logo">
                                     <div className="row align-items-center">
                                         <div className="col-auto">
@@ -25,20 +75,34 @@ class WeatherApp extends Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-6">
+                            <div className="col-12 col-md-6">
                                 <div className="WeatherApp-search">
-                                    <input type="text" className="input" placeholder="Miasto" />
-                                    <i className="fas fa-search"></i>
+                                    <form onSubmit={this.handleSubmit}>
+                                        <input type="text" className="input input-search" placeholder="Miasto" value={this.state.value} onChange={this.handleChange} />
+                                        <button type="submit" className="button button-search"><i className="fas fa-search"></i></button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </header>
                 <div className="WeatherApp-content">
-                    <i className="wi wi-day-sunny"></i>
+                    <div className="container">
+                        <div className="row align-items-top">
+                            {this.renderLoading()}
+                            {this.renderWeather()}
+                            {this.renderWeatherBlocks()}
+                        </div>
+                    </div>
                 </div>
                 <footer className="WeatherApp-footer">
-
+                    <div className="container">
+                        <div className="row align-items-center">
+                            <div className="col">
+                                {/* <p>&copy; Wojciech Bubolka</p> */}
+                            </div>
+                        </div>
+                    </div>
                 </footer>
             </div>
         );
@@ -48,5 +112,5 @@ export default geolocated({
     positionOptions: {
         enableHighAccuracy: false,
     },
-    userDecisionTimeout: 5000,
+    userDecisionTimeout: 15000,
 })(WeatherApp);
--- src/components/WeatherBlock.js
+++ src/components/WeatherBlock.js
@@ -1,19 +1,93 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+import 'moment/locale/pl';
 import './WeatherBlock.css';
+import moment from 'moment';
 
 class WeatherBlock extends Component {
-    render() {
+    calculate(deg) {
+        return Math.ceil(deg);
+    }
+    renderConditionIcon() {
+        const weather = this.props.item.weather[0].main;
+        // const weatherId = this.props.item.weather[0].id;
+        const hour = moment(this.props.item.dt_txt).format('h');
+        const time = (hour < 7 || hour > 19) ? 'night' : 'day';
+        // const time = 'day';
+        let condition;
+        switch (weather) {
+            case 'Thunderstorm':
+            condition = time === 'day' ? 'day-thunderstorm' : 'night-thunderstorm';
+                break;
+            case 'Drizzle':
+                condition = time === 'day' ? 'day-rain-mix' : 'night-rain-mix';
+                break;
+            case 'Rain':
+                condition = time === 'day' ? 'day-rain' : 'night-rain';
+                break;
+            case 'Snow':
+                condition = time === 'day' ? 'day-snow' : 'night-snow';
+                break;
+            case 'Atmosphere':
+                condition = time === 'day' ? 'day-fog' : 'night-fog';
+                break;
+            case 'Clear':
+                condition = time === 'day' ? 'day-sunny' : 'night-clear';
+                break;
+            case 'Clouds':
+                condition = time === 'day' ? 'day-cloudy' : 'night-cloudy';
+                break;
+            default:
+                condition = 'rain';
+                break;
+        }
         return (
-            <div className="WeatherBlock">
-                <header className="WeatherBlock-header">
-
-                </header>
-                <div className="WeatherBlock-content">
-
+            <i className={`wi wi-${condition}`}></i>
+        )
+    }
+    render() {
+        //console.log(this.props.item.main.temp);
+        return (
+            <div className="col-6 col-sm-4 col-md-3 col-lg-3 my-3">
+                <div className="WeatherBlock animated fadeIn slow p-3">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="WeatherBlock-date">
+                                <div className="WeatherBlock-date--day">
+                                    <Moment locale="pl" format="LT dddd">{this.props.item.dt_txt}</Moment>
+                                </div>
+                                <div className="WeatherBlock-date--full">
+                                    <Moment locale="pl" format="LL">{this.props.item.dt_txt}</Moment>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row no-gutters">
+                        <div className="col">
+                            <div className="WeatherBlock-condition">
+                                {this.renderConditionIcon()}
+                            </div>
+                            <div className="WeatherBlock-temp">
+                                {this.calculate(this.props.item.main.temp)}&deg;<span className="deg">C</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="WeatherBlock-description mb-1">
+                            {this.props.item.weather[0].description}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="WeatherBlock-wind">
+                                <i className="wi wi-wind-direction" style={ {transform: `rotate(${this.props.item.wind.deg}deg)`}}></i> Wiatr: {this.props.item.wind.speed} m/s
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
-                <footer className="WeatherBlock-footer">
-
-                </footer>
             </div>
         );
     }
